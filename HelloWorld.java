007f8d0f958d3a8361da59c0fe6993f52c0e0fba
rameshnarsingu1 <149674447+rameshnarsingu1@users.noreply.github.com>
2023-11-07 06:42:14 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("Hello, Ramesh you want to learn jenkins");
+    System.out.println("Hello, HitHub hook updated in jenkins");
   }
 }
